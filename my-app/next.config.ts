f8db1cf7766c9d05647f3b7aaf2eea3e40db691c
Ju-Min-Kyung <alsrud7767@naver.com>
2025-07-26 23:53:23 +0900

    feat: get info of feed

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd34u8crftukxnk.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+      // 기존에 등록한 도메인도 여기 포함해야 합니다.
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
